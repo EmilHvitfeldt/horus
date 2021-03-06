cb3a4d21c5430b7f29e58d30588413d99900be32
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-03-05 22:08:36 -0800

    reorder importfrom

--- R/0_imports.R
+++ R/0_imports.R
@@ -1,26 +1,29 @@
-#' @importFrom tidyr pivot_longer drop_na nest unnest
-#' @importFrom dplyr mutate everything desc filter bind_cols all_of select
-#' @importFrom dplyr group_by summarize arrange slice pull count row_number
-#' @importFrom dplyr select_ mutate_ recode
-#' @importFrom tibble tibble rownames_to_column as_tibble
-#' @importFrom rlang .data enquo quo_name set_names ensym eval_tidy abort
-#' @importFrom parsnip augment
-#' @importFrom ggplot2 labs ggplot aes geom_point geom_abline theme_minimal
-#' @importFrom ggplot2 geom_raster aes_string scale_fill_gradient2 guide_legend
-#' @importFrom ggplot2 geom_text geom_histogram facet_grid geom_bar aes_
-#' @importFrom ggplot2 scale_y_discrete xlim guides unit arrow geom_segment
+#' @importFrom dplyr all_of arrange bind_cols count desc everything filter
+#' @importFrom dplyr group_by mutate mutate_ pull recode row_number select
+#' @importFrom dplyr select_ slice summarize
+#' @importFrom forcats fct_infreq fct_lump fct_relevel fct_rev
+#' @importFrom ggplot2 aes aes_ aes_string arrow facet_grid geom_abline
+#' @importFrom ggplot2 geom_bar geom_histogram geom_point geom_raster
+#' @importFrom ggplot2 geom_segment geom_text ggplot guide_legend guides labs
+#' @importFrom ggplot2 scale_fill_gradient2 scale_y_discrete theme_minimal unit
+#' @importFrom ggplot2 xlim
 #' @importFrom glue glue
-#' @importFrom stats prcomp var predict
-#' @importFrom readr parse_number
-#' @importFrom forcats fct_relevel fct_rev fct_infreq fct_lump
+#' @importFrom magrittr %>%
+#' @importFrom parsnip augment
 #' @importFrom purrr map
+#' @importFrom readr parse_number
+#' @importFrom rlang .data abort enquo ensym eval_tidy quo_name set_names
 #' @importFrom Rtsne Rtsne
+#' @importFrom stats prcomp predict var
+#' @importFrom tibble as_tibble rownames_to_column tibble
+#' @importFrom tidyr drop_na nest pivot_longer unnest
+#' @importFrom utils globalVariables
+NULL
 #'
 
 # ------------------------------------------------------------------------------
 # nocov
 
-#' @importFrom utils globalVariables
 utils::globalVariables(
   c(".pred", ".resid", "name", "value", "contribution")
 )
--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -6,6 +6,5 @@
 #' @rdname pipe
 #' @keywords internal
 #' @export
-#' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
 NULL
