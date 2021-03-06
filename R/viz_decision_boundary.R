69dd85101acc7b433330e4e00050c1be2aa96b90
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-03-05 23:16:25 -0800

    guarding and refactor

--- R/viz_decision_boundary.R
+++ R/viz_decision_boundary.R
@@ -1,13 +1,11 @@
 #' Draw Decision boundary for Classification model
 #'
 #' This function is mostly useful in an educational setting. Can only be used
-#' with fitted workflow objects with 2 numeric predictor variables.
+#' with trained workflow objects with 2 numeric predictor variables.
 #'
-#' @param x fitted `workflows::workflow` object.
+#' @param x trained `workflows::workflow` object.
 #' @param new_data A data frame or tibble for whom the preprocessing will be
 #'   applied.
-#' @param grid Logical, whether to use grid method to show decision boundary.
-#'   Defaults to TRUE.
 #' @param resolution Number of squared in grid. Defaults to 100.
 #' @param expand Expansion rate. Defaults to 0.1. This means that the width and
 #'   height of the shaded area is 10% wider then the rectangle containing the
@@ -21,18 +19,25 @@
 #' @examples
 #' library(parsnip)
 #' library(workflows)
-#' lm_spec <- svm_rbf() %>%
+#' svm_spec <- svm_rbf() %>%
 #'   set_mode("classification") %>%
 #'   set_engine("kernlab")
 #'
-#' lr_fit <- workflow() %>%
+#' svm_fit <- workflow() %>%
 #'   add_formula(Species ~ Petal.Length + Petal.Width) %>%
-#'   add_model(lm_spec) %>%
+#'   add_model(svm_spec) %>%
 #'   fit(iris)
 #'
-#' viz_decision_boundary(lr_fit, iris)
-viz_decision_boundary <- function(x, new_data, grid = TRUE, resolution = 100,
-                                  expand = 0.1) {
+#' viz_decision_boundary(svm_fit, iris)
+viz_decision_boundary <- function(x, new_data, resolution = 100, expand = 0.1) {
+
+  if (!inherits(x, "workflow")) {
+    abort("`viz_decision_boundary()` only works with `workflow` objects.")
+  }
+  if (!x$trained) {
+    abort("`x` must be a trained `workflow` object.")
+  }
+
   if (names(x$pre$actions) == "variables") {
     predictors <- eval_tidy(
       x$pre$actions$variables$predictors,
@@ -59,30 +64,33 @@ viz_decision_boundary <- function(x, new_data, grid = TRUE, resolution = 100,
 
   predict_area <- new_data %>%
     select(all_of(predictors)) %>%
-    lapply(function(x) {
-      x_range <- range(x, na.rm = TRUE)
-
-      x_range_width <- x_range[2] - x_range[1]
-
-      sequence <- seq(
-        from = x_range[1] - x_range_width * expand / 2,
-        to = x_range[2] + x_range_width * expand / 2,
-        length.out = resolution
-      )
-
-      if (is.integer(x)) {
-        sequence <- unique(as.integer(sequence))
-      }
-      sequence
-    }) %>%
+    lapply(expanded_seq, expand, resolution) %>%
     expand.grid()
 
   predict_area %>%
     bind_cols(predict(x, predict_area)) %>%
     ggplot(aes_string(predictors[1], predictors[2], fill = ".pred_class")) +
     geom_raster(alpha = 0.2) +
-    geom_point(aes_string(predictors[1], predictors[2], fill = response),
+    geom_point(
+      aes_string(predictors[1], predictors[2], fill = response),
       color = "black", shape = 22, data = new_data, inherit.aes = FALSE
     ) +
     theme_minimal()
+}
+
+expanded_seq <- function(x, expand, resolution) {
+  x_range <- range(x, na.rm = TRUE)
+
+  x_range_width <- x_range[2] - x_range[1]
+
+  sequence <- seq(
+    from = x_range[1] - x_range_width * expand / 2,
+    to = x_range[2] + x_range_width * expand / 2,
+    length.out = resolution
+  )
+
+  if (is.integer(x)) {
+    sequence <- unique(as.integer(sequence))
+  }
+  sequence
 }
