e3cba47ec2c7e007ac835ef0f67bf0dfd49b7cfb
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-03-12 19:36:47 -0800

    add mnist_sample

--- R/data.R
+++ R/data.R
@@ -5,3 +5,17 @@
 #'
 #' @format A tibble with 486 obs. of 2 variables.
 "fairy_tales"
+
+#' Down-sampled MNIST
+#'
+#' this data set is a down-sampled version of the MNIST data set which have been
+#' umap'ed down to 2 dimensions.
+#'
+#' @source \url{http://yann.lecun.com/exdb/mnist/}
+#' @format A data frame with 1200 rows and 3 variables:
+#' \describe{
+#'   \item{class}{Factor, values 0 through 9.}
+#'   \item{umap_1}{Numeric.}
+#'   \item{umap_2}{Numeric.}
+#' }
+"mnist_sample"
